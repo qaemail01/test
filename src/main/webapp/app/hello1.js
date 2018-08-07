20d28a73a66d0a98b2de3439ded91d0b720ba436
Max B <qaemail01@gmail.com>
2018-08-07 18:42:23 +0300

    add input control

--- src/main/webapp/app/hello1.js
+++ src/main/webapp/app/hello1.js
@@ -33,3 +33,34 @@ class ShoppingList extends React.Component {
 }
 
 ReactDOM.render(<ShoppingList/>, document.getElementById('app'));
+
+class Timer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { seconds: 0 };
+    }
+
+    tick() {
+        this.setState(prevState => ({
+            seconds: prevState.seconds + 1
+        }));
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        return (
+            <div>
+                Seconds: {this.state.seconds}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Timer />, document.getElementById('timer'));
