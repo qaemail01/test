97697be685bf1e24d8bced97958c0f473080f611
Max B <qaemail01@gmail.com>
2018-08-07 17:59:22 +0300

    add input control

--- src/main/webapp/app/hello1.js
+++ src/main/webapp/app/hello1.js
@@ -2,18 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class ShoppingList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: 'type here'};
+        this.items = [{id: "1", text: "Instagram"}, {id:"2", text:"WhatsApp"}, {id:"3", text:"Oculus"}];
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({text : e.target.value});
+    }
+
     render() {
         return (
             <div className="shopping-list">
-                <h1>Shopping List for {this.props.name}</h1>
+                <h1>Shopping List for {this.state.text}</h1>
                 <ul>
-                    <li>Instagram</li>
-                    <li>WhatsApp</li>
-                    <li>Oculus</li>
+                    {this.items.map(item => (
+                        <li key={item.id}>{item.text}</li>
+                    ))}
                 </ul>
+                <input
+                    id="new-todo"
+                    onChange={this.handleChange}
+                    value={this.state.text}
+                />
             </div>
         );
     }
 }
 
-ReactDOM.render(<ShoppingList name="Maximus"/>, document.getElementById('app'));
+ReactDOM.render(<ShoppingList/>, document.getElementById('app'));
